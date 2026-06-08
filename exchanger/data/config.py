5569fcd7a5e761f9cb60b3c39afc0ae57aebdc13
mskdomservice-design <mskdomservice@gmail.com>
2026-06-08 22:15:16 +0300

    1

---
+++ exchanger/data/config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = '8085705066:AAFOxs0pfdnnLU0IBTcwcNUygDi_IZ8eYcA'
+ADMIN_ID = [8325314043]
+ANKET_SEND = [8325314043]
+SUPPORT_LINK = 'https://t.me/'
+PATH_TO_DB = '/app/data/'
+LINK_SUBSCRIBE = 'https://t.me/'
+FAQ = 'Часто задаваемые вопросы: Разработчики: https://t.me/'
